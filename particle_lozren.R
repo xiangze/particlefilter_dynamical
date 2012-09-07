48a713686446d51c6d71009848a866f2c7d7fcfd
xiangze <xiangze@gmail.com>
2012-09-08 03:03:11 +0900

    debug particle filter.

--- particle_lozren.R
+++ particle_lozren.R
@@ -1,16 +1,17 @@
-#library(odesolve)
-#source("lotz.f.noise.R")
+source("lotz.f.noise.R")
+source("rossler.noise.R")
+source("resample.R")
 
-dt <- 0.01
+dt <- 0.001
 N <- 30
-times <- 2000
-interval <- 20
+times <- 4000
+interval <- 10
 
 lag <- 1
 
-rv <- 0.01 #original noise
-xv <- 0.1  #system noise var
-yv <- 3.   #obesebation noise var
+rv <- 0.001 #original noise
+xv <- 0.01  #system noise var
+yv <- 0.3   #obesebation noise var
 nv <- 0.01 # of PF
 
 psize <- c(-40,40)
@@ -21,62 +22,39 @@ lotz.params <- list(dl = 10,
                bt = 8/3,
                ro = 28)
 
-lotz.noise <- function(t, y,v,p=lotz.params){
-  y <- y+rnorm(3,v)
-  dl <- p[['dl']]
-  ro <- p[['ro']]
-  bt <- p[['bt']]
+for(N in c(10)){
+  for(ro in c(331)){
+#  for(ro in c(16,28,200)){  
 
-       c(dl * (y[2] - y[1]),
-           y[1] * (ro - y[3]) - y[2],
-           y[1] * y[2] - bt * y[3] )
-}
-
-
-resample<- function(inits,costs){
-   inits.n <- c()
-    l <- length(costs)
-
-   for (j in 1:l){
-      nn <- costs[j]
-
-      if(nn>=1)
-        for(kk in 1:nn){
-          inits.n <- rbind(inits.n,inits[j,])
-        }
-
-      if((nn%%1)>runif(1,0,1)){
-        inits.n <- rbind(inits.n,inits[j,])
-      }
-    }
-   inits.n <- rbind(inits.n,inits.n)
-   inits.n <-inits.n[1:l,]
-}
-
-lz <- function(x,v){x+dt*lotz.noise(0,x,v)}
-#f <- function(x){x+dt*lotz.noise(0,x,xv)}
+    lotz.params$c <- ro
+    lz <- function(x,v){x+dt*lotz.noise(0,x,v,lotz.params)}
+    
+#    lotz.params$ro <- ro
+#    lz <- function(x,v){x+dt*lotz.noise(0,x,v,lotz.params)}
+    
+    pname <-paste(N,lotz.params$ro,interval,".png",sep="_")
 
 #make original(hidden) time series (x) with system noise
 #observed sequence :y
-y <- array(dim=c(times/interval,3))
-xo <- array(dim=c(1,3))
-xo[1,] <- c(0.2,0.2,0.1)
+    y <- array(dim=c(times/interval,3))
+    xo <- array(dim=c(1,3))
+    xo[1,] <- c(0.2,0.2,0.1)
 
 for (t in 2:times){  
   xo <- rbind(xo,lz(xo[t-1,],rv))
   if(t%%(interval)==0){
-    y[t/interval,] <-xo[t,]+rnorm(3,yv);
+    y[t/interval,] <-xo[t,]+rnorm(3,0,yv);
   }
 }
 
 if(preshow){
-  png("org_phase.png")
+  png(paste("org_phase",pname,sep="_"))
   plot(y[,2],y[,3], type='l',xlim=psize,ylim=psize,col="red",xlab="x",ylab="y")
   par(new=T)
   plot(xo[,2],xo[,3], type='l',xlim=psize,ylim=psize, axes=F,xlab="",ylab="")
   dev.off()
 
-  png("org_time.png")
+  png(paste("org_time",pname,sep="_"))
   plot(seq(1,times),xo[,2], type='l', axes=F,xlab="",ylab="")
   par(new=T)
   plot(seq(1,times,by=interval),y[,2],type='l',col="red",xlab="x",ylab="y")
@@ -112,7 +90,7 @@ for (t in 2:times){
 }
 
 if(show){
-  png("particles_phase.png")
+  png(paste("particle_phase",N,lotz.params$ro,interval,".png",sep="_"))
   for (i in 1:N){
     plot(x[,i,2],x[,i,3], type='l',xlim=psize,ylim=psize, axes=F,xlab="",ylab="")
     par(new=T)
@@ -120,7 +98,7 @@ if(show){
   plot(y[,2],y[,3], type='l',xlim=psize,ylim=psize,col="red",ann=T)
   dev.off()
 
-  png("particles_time.png")
+  png(paste("particle_time",N,lotz.params$ro,interval,".png",sep="_"))
   for (i in 1:N){
     plot(seq(1,times),x[,i,2], type='l', axes=F,xlab="",ylab="")
     par(new=T)
@@ -128,3 +106,7 @@ if(show){
   plot(seq(1,times,by=interval),y[,2],type='l',col="red",ann=T)
   dev.off()
 }
+  }
+}
+
+
