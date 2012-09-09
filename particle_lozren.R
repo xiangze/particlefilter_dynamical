3324ed7141379325d0ce17642cc071b6264014f8
xiangze <xiangze@gmail.com>
2012-09-10 02:38:50 +0900

    weight is debbuged. variance,difference plots are added.

--- particle_lozren.R
+++ particle_lozren.R
@@ -4,41 +4,35 @@ source("resample.R")
 
 dt <- 0.02
 N <- 100
-times <- 5000
-interval <- 50
+times <- 10000
+interval <- 100
 
 lag <- 1
 
-rv <- 0.0 #original noise
+rv <- 0.0 #original noise var
 xv <- 0.01  #system noise var
 yv <- 0.03   #obesebation noise var
-nv <- 0.001 # of PF
+nv <- 0.001 # dispersion of PF
 
-psize <- c(-40,40)
+psize <- c(-100,100)
 preshow <- T
 show <- T
 
-lotz.params <- list(dl = 10,
-               bt = 8/3,
-               ro = 28)
-
-rossler.params <- list(a = 0.1,
-               b = 0.1,
-               c = 18)
-
-for(N in c(30)){
-  for(ro in c(4,5.7,14)){
+for(N in c(100)){
+      for(ro in c(4,5.7,14,18)){
 #  for(ro in c(331)){
-#  for(ro in c(16,28,200)){  
+#  for(ro in c(16,28,331)){  
 
     rossler.params$c <- ro
     lz <- function(x,v){x+dt*rossler.noise(0,x,v,rossler.params)}
-    pname <-paste("ros",N,ro,interval,".png",sep="_")
+    pname <-paste("ros","N",N,ro,"int",interval,".png",sep="_")
     
 #    lotz.params$ro <- ro
 #    lz <- function(x,v){x+dt*lotz.noise(0,x,v,lotz.params)}
-#    pname <-paste(N,lotz.params$ro,interval,".png",sep="_")
+#    pname <-paste("loz","N",N,ro,"int",interval,".png",sep="_")
 
+
+    
 #make original(hidden) time series (x) with system noise
 #observed sequence :y
     y <- array(dim=c(times/interval,3))
@@ -53,12 +47,12 @@ for (t in 2:times){
 }
 
 if(preshow){
-##   png(paste("org_phase",pname,sep="_"))
-##   plot(y[,2],y[,3], type='l',xlim=psize,ylim=psize,col="red",xlab="x",ylab="y")
-##   par(new=T)
-##   plot(xo[,2],xo[,3], type='l',xlim=psize,ylim=psize, axes=F,xlab="",ylab="")
-##   dev.off()
-
+  png(paste("org_phase",pname,sep="_"))
+  plot(xo[,2],xo[,3], type='l',xlim=psize,ylim=psize, axes=F,xlab="",ylab="")
+  par(new=T)  
+  plot(y[,2],y[,3], type='l',xlim=psize,ylim=psize,col="red",xlab="x",ylab="y")
+  dev.off()
+  
   png(paste("org_time",pname,sep="_"))
   plot(seq(1,times),xo[,2], type='l', axes=F,xlab="",ylab="")
   par(new=T)
@@ -68,27 +62,29 @@ if(preshow){
 }
 
 #prediction by particles 
-x <- array(dim=c(times,N,3))
-x[1,,] <- matrix(runif(N,-0.2,0.2))
+    x <- array(dim=c(times,N,3))
+    x[1,,] <- matrix(runif(N,-0.2,0.2))
 
-for (t in 2:times){
-  for(n in 1:N){
-    x[t,n,] <- lz(x[t-1,n,],xv)
-  }
+    for (t in 2:times){
+
+      for(n in 1:N){
+        x[t,n,] <- lz(x[t-1,n,],xv)
+      }
     
   if(t%%(interval)==0 && t/interval>lag){
     weight <- rep(0,N) 
     for(n in 1:N){
       for(i in 0:(lag-1)){
-        weight[n] <- weight[n]-dnorm(y[(t/interval-i),],x[(t-i*interval),n,],yv,log=T)
+        weight[n] <- weight[n]+dnorm(y[(t/interval-i),],x[(t-i*interval),n,],yv,log=T)
       }
     }
 #    print(weight)
 #    browser()
+
     wm <- max(weight)
     weight <- sapply(weight,function(x){exp(x-wm)})
-                                            
 #    print(wm)
+    
     costs <- weight/sum(weight)*N
     x[t,,] <- resample(x[t,,],costs)
     x[t,,] <- x[t,,]+matrix(rnorm(length(x[t,,]),0,nv),nc=ncol(x[t,,]))
@@ -96,13 +92,14 @@ for (t in 2:times){
 }
 
 if(show){
-##   png(paste("particle_phase",pname,sep="_"))
-##   for (i in 1:N){
-##     plot(x[,i,2],x[,i,3], type='l',xlim=psize,ylim=psize, axes=F,xlab="",ylab="")
-##     par(new=T)
-##   }
-##   plot(y[,2],y[,3], type='l',xlim=psize,ylim=psize,col="red",ann=T)
-##   dev.off()
+  
+  png(paste("particle_phase",pname,sep="_"))
+  for (i in 1:N){
+    plot(x[,i,2],x[,i,3], type='l',xlim=psize,ylim=psize, axes=F,xlab="",ylab="")
+    par(new=T)
+  }
+  plot(y[,2],y[,3], type='l',xlim=psize,ylim=psize,col="red",ann=T)
+  dev.off()
 
   png(paste("particle_time",pname,sep="_"))
   for (i in 1:N){
@@ -111,18 +108,31 @@ if(show){
   }
   plot(seq(1,times,by=interval),y[,2],type='l',col="red",ann=T)
   dev.off()
-
-  xa1 <- array(dim=times)
-  for (t in 1:times){
-    xa1[t] <- sum(x[t,,2])
-  }
   
-  png(paste("particle_time_A",pname,sep="_"))
-  plot(seq(1,times),xa1-y[,2], type='l')
+##   xm <- array()
+##   xv <- array()
+##   for(ta in seq(1,times,by=interval)){
+##     xm <- rbind(xm,mean(x[ta,,2]))
+##     xv <- rbind(xv,var(x[ta,,2]))
+##   }
+  
+  xm <- array(dim=times/interval)
+  xv <- array(dim=times/interval)
+  for(ta in seq(1,times,by=interval)){
+    xm[ta/interval] <- mean(x[ta,,2])
+    xv[ta/interval] <- var(x[ta,,2])
+  }
+
+  png(paste("particle_time_dif",pname,sep="_"))
+  plot(seq(1,times/interval),xm-y[,2], type='l')
   dev.off()
- 
+
+  png(paste("particle_time_var",pname,sep="_"))
+  plot(seq(1,times/interval),xv, type='l')
+  dev.off()
+
 }
-    
+   
   }
 }
 
